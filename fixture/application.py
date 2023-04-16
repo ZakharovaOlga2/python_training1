9496655c2c29501912191af0e8af4b7dd4fcdaec
Zakharova Olga <olga.zaharova@im.systems>
2023-04-16 23:14:15 +0300

    Выделили вспомогательный класс SessionHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,15 +1,13 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
 
 class Application:
+
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
 
     def add_new_group(self, group):
         wd = self.wd
@@ -39,16 +37,6 @@ class Application:
     def header_menu_navigation(self, item):
         wd = self.wd
         wd.find_element_by_link_text(item).click()
-
-    def login(self, login, password):
-        wd = self.wd
-        self.go_to_home_page("http://localhost/addressbook/")
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(login)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def go_to_home_page(self, url):
         wd = self.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,13 +10,13 @@ def app(request):
     return fixture
 
 def test_add_group_py(app):
-    app.login(login="admin", password="secret")
+    app.session.login(login="admin", password="secret")
     app.add_new_group(Group("21212", "vsdvsdvsd", "ebrbsf d"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group_py(app):
-    app.login( login="admin", password="secret")
+    app.session.login( login="admin", password="secret")
     app.add_new_group(Group("", "", ""))
-    app.logout()
+    app.session.logout()
 
 
