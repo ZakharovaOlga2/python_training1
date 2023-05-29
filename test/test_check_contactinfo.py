391eb646c943bbac6367f2d1e7849f8aad2023f1
Zakharova Olga <olga.zaharova@im.systems>
2023-05-29 18:56:48 +0300

    Генератор и загрузка тестовых данных

--- test/test_check_contactinfo.py
+++ test/test_check_contactinfo.py
@@ -1,42 +1,20 @@
 import re
 
-def test_email_on_home_page(app):
+def tests_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_emails == merge_emails_like_home_page(contact_from_edit_page)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_home_page(contact_from_edit_page)
+    assert contact_from_home_page.address == contact_from_edit_page.address
 
-def test_email_on_contact_view_page(app):
+def test_on_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_view_page.all_emails == merge_emails_like_home_page(contact_from_edit_page)
-
-def test_names_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-
-def test_names_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_view_page.full_name == get_full_name(contact_from_edit_page)
-
-def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_home_page(contact_from_edit_page)
-
-def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(3)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(3)
     assert merge_phones_like_home_page(contact_from_view_page) == merge_phones_like_home_page(contact_from_edit_page)
-
-def test_address_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[3]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(3)
-    assert contact_from_home_page.address == contact_from_edit_page.address
-
-def test_address_on_contact_view_page(app):
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     if contact_from_edit_page.address != "":
         assert app.contact.get_fullinfo_from_view_page(0).find(contact_from_edit_page.address) > 0
