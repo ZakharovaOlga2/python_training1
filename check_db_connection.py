b93d269a9ebef62a4a2f0e663a51f266b75f4a38
Zakharova Olga <olga.zaharova@im.systems>
2023-05-30 20:59:08 +0300

    Проверки с использованием списков из БД

---
+++ check_db_connection.py
@@ -0,0 +1,11 @@
+from fixture.orm import ORMFixture
+from model.group import Group
+
+db = ORMFixture(host="127.0.0.1",name="addressbook",user="root",password="")
+try:
+    lst = db.get_contacts_not_in_group(Group(id="11"))
+    for item in lst:
+        print(item)
+    print(len(lst))
+finally:
+    pass
