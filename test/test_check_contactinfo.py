dff7ee05613738b948d28f5c811850a5b34f436e
Zakharova Olga <olga.zaharova@im.systems>
2023-05-31 15:37:49 +0300

    test_check_all_view_contacts_with_db переработана проверка

--- test/test_check_contactinfo.py
+++ test/test_check_contactinfo.py
@@ -26,8 +26,22 @@ def test_on_view_page(app):
 def test_check_all_view_contacts_with_db(app, db):
     db_contacts = db.get_contact_list()
     view_contacts = app.contact.get_contact_list()
-    assert db_contacts == sorted(view_contacts,key=Contact.id_or_max)
+    compare_contact_list_info(sorted(db_contacts,key=Contact.id_or_max),sorted(view_contacts,key=Contact.id_or_max))
 
+def compare_contact_list_info(list1,list2):
+    error = ""
+    if len(list1) != len(list2):
+        assert False, "Не совпадает количество контактов в БД и из интерфейса"
+    counter = 0
+    for contact_list1 in list1:
+        contact_list2 = list2[counter]
+        if contact_list1.all_emails == contact_list2.all_emails and contact_list1.firstname == contact_list2.firstname and contact_list1.lastname == contact_list2.lastname and contact_list1.all_phones_from_home_page == contact_list2.all_phones_from_home_page and contact_list1.all_emails == contact_list2.all_emails and contact_list2.address == contact_list1.address:
+            pass
+        else:
+            error = error+"\nНе совпадают данные в элементе %s " % counter
+        counter = counter + 1
+    if error !="":
+        assert False, error
 
 def get_full_name(contact):
     result = " ".join(filter(lambda x: x != "", filter(lambda x: x is not None,
